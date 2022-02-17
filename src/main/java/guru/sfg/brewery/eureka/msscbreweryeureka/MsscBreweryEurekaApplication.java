6b220621c99a407f10e217973a5d86881757a7f3
Philippe <philippe@igor-brevnjovski.net>
2022-02-17 16:04:53 +0100

    first commit

---
+++ src/main/java/guru/sfg/brewery/eureka/msscbreweryeureka/MsscBreweryEurekaApplication.java
@@ -0,0 +1,13 @@
+package guru.sfg.brewery.eureka.msscbreweryeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBreweryEurekaApplication.class, args);
+	}
+
+}
